3589fd350de3acb2aff2e685230f7a6673779a78
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-08 14:41:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function makeInt(n){
 function preserveDecimal(n){
   return parseFloat(n)
 }
-/*function preserveDecimal(n){
-  return NaN;
-}*/
+function preserveDecimal(n){
+  return parseInt(n, 10);
+}
