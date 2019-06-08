4ec858d56e9dd20550c86aed936f5207cf1f6bba
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-08 14:28:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function add(a, b){
+  result = a + b;
+  return result; 
+}
+function subtract(a, b){
+  result = a - b;
+  return result;
+}
+function multiply(a, b){
+  result = a * b;
+  return result;
+}
+function divide(a, b){
+  result = a / b;
+  return result; 
+}
